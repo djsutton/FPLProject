49e407ae0f459f6dd3719173eec81b7102a28579
Matt Ripley <matt@engr2-1-193-53-dhcp.int.colorado.edu>
2011-12-05 14:14:51 -0700

    Removed se types and context types

--- ast.ml
+++ ast.ml
@@ -48,22 +48,6 @@ type value =
   | Funval of var * exp
   | CnkVal of builtIn * se list
 
-(* "Simple" expressions as defined in the paper *)
-and se = 
-  | Value of value
-  | Ident of var  
-
-type ctx = 
-  | Hole
-  | ApplyLeftCtx of ctx * exp
-  | ApplyRightCtx of exp * ctx
-  | CondCtx of ctx * exp * exp
-
-type sctx = 
-  | Hole
-  | BindCtx of var * ctx 
-  | ParCtx of sctx * sctx
-
 (* Functions to convert AST into a string *)
 let string_of_const c =
   match c with
